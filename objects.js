e1feed3e958752779465194a6b028a852b0985ac
MAHAK RAJVANSHI <mahakrajvanshi@yahoo.com>
2017-12-28 04:09:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,6 @@
 var playlist = {'Phil Ochs': 'Here\'s to the State of Mississippi'}
+
+function updatePlaylist(playlist, artistName, songTitle){
+    playlist[artistName] = aongTitle
+    return playlist
+}
